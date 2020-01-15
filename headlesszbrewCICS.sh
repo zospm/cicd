1b7bbca84f4da77610707508719af359259fb8c1
Mike Fulton <fultonm@ca.ibm.com>
2020-01-15 10:18:54 -0600

    fix typo

--- headlesszbrewCICS.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Run this program so that you won't be prompted for password except at start
-#
-(
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-nohup zbrewCICD.sh >/tmp/zbrew-cicd.out 2>&1 &
-)
