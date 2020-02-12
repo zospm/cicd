49c2c2d7daac0689c7989e39ada65aa44ef56ee6
Mike Fulton <fultonm@ca.ibm.com>
2020-02-12 16:16:18 -0500

    finishing off #68. Cleaned up the code significantly so that all errors should now be reported to slack

--- zbrewCICD.sh
+++ zbrewCICD.sh
@@ -5,23 +5,26 @@
 verbose=0
 
 function SlackMsg {
-	repo=$1
-	rc=$2
-	msg=$3
-	file=$4
-	log=$5
+	msg=$1
 
 	url="${SLACK_MSG_URL}"
-	rm -f "${log}"
-	curl -k -X POST -H "'Content-type: application/json'" --data "{\"text\":\"${msg}\"}" "${url}" >${log} 2>&1
+	curl -ks -X POST -H "'Content-type: application/json'" --data "{\"text\":\"${msg}\"}" "${url}" >/dev/null 2>&1
+	rc=$?
+	if [ $rc -gt 0 ]; then
+		echo "curl failed to post message to slack url: ${url}" >&2
+	fi
 
 	return 0
 }
 
 function RepoStatus {
 	repo=$1
+	gitout=gitfetch${repo}.out
 	if [ -d .git ]; then
-		git fetch >"${repo}_gitfetch.results" 2>&1
+		git fetch >"${gitout}" 2>&1
+		if [ $? -eq 0 ]; then
+			rm -f ${gitout}
+		fi
 		REMOTE=`git rev-parse @{u}`
 		LOCAL=`git rev-parse HEAD`
 
@@ -32,152 +35,152 @@ function RepoStatus {
 		fi
 	else 
 		cd ../
-		git clone "${GIT_OWNER}@${GIT_SERVER}:${GIT_USER}/${repo}.git" >"${repo}_gitclone.results" 2>&1
+		git clone "${GIT_OWNER}@${GIT_SERVER}:${GIT_USER}/${repo}.git" >"${gitout}" 2>&1
+		if [ $? -eq 0 ]; then
+			rm -f ${gitout}
+		fi
 		cd ${repo}
 		echo "PULL"
 	fi
 	return 0
 }
 
-function StepMsg {
-	status=$1
-	step=$2
-        repo=$3
-	rc=$4
-	file=$5
-	log=$6
-	slack=$7
-
-	if [ ${slack} -gt 0 ]; then 
-		SlackMsg "${repo}" "${rc}" "${step} ${status} with rc: ${rc}" "${file}" "${log}"
-	fi
-	echo "${status}"
-}
-
 function RepoBuild {
 	repo=$1
+	gitout=gitpull.out
 	out="RepoBuild.results"
-	log="build.log"
-	rm -f ${out}
-	touch ${out}
-	git pull "${GIT_OWNER}@${GIT_SERVER}:${GIT_USER}/${repo}.git" >"gitpull.results" 2>&1
+
+	git pull "${GIT_OWNER}@${GIT_SERVER}:${GIT_USER}/${repo}.git" >"${gitout}" 2>&1
 	rc=$?
 	if [ ${rc} -ne 0 ]; then
-		echo "FAIL"
+		echo "RepoBuild: git pull ${repo} failed with rc:$rc"
 		return ${rc}
+	else
+		rm -f "${gitout}"
 	fi
-	./build.sh >>${out} 2>&1
+
+	./build.sh >${out} 2>&1
 	rc=$?
 	if [ ${rc} -ne 0 ]; then
-		StepMsg "FAIL" "Repository Build" "${repo}" "${rc}" "${out}" "${log}" 1
-	else 
-		StepMsg "PASS" "Repository Build" "${repo}" "${rc}" "${out}" "${log}" ${verbose}
+		echo "RepoBuild: ${repo} build failed with rc:$rc"
 	fi
-	return 0
+	return $rc
 }
 
 
 function RepoTest {
 	repo=$1
 	out="RepoTest.results"
-	log="curl_test.log"
-	rm -f ${out}
-	touch ${out}
+
 	./test.sh >${out} 2>&1
 	rc=$?
 	if [ ${rc} -ne 0 ]; then
-		StepMsg "FAIL" "Repository Test" "${repo}" "${rc}" "${out}" "${log}" 1
-	else 
-		StepMsg "PASS" "Repository Test" "${repo}" "${rc}" "${out}" "${log}" ${verbose}
+		echo "RepoTest: ${repo} test failed with rc: $rc"
+	else
+		rm -f ${out}
 	fi
-	return 0
+	return $rc
 }
 
 function RepoDeploy {
-	out="RepoDeploy.results"
-	log="curl_deploy.log"
-	rm -f ${out}
-	touch ${out}
 	repo=$1
 	timestamp=$2
 	hash=$3
 	paxfile=$4
 	artifact_url=$5
+
+	out="RepoDeploy.results"
+	curlout="curl.out"
 	rm -rf "${DEPLOY_ROOT}/${repo}"
+
 	mkdir -p "${DEPLOY_ROOT}/${repo}"
 	rc=$?
 	if [ ${rc} -gt 0 ]; then
-		echo "Unable to create deploy directory: ${DEPLOY_ROOT}/${repo}" >>${out}
+		echo "RepoDeploy: Unable to create deploy directory: ${DEPLOY_ROOT}/${repo}. rc:$rc"
 		return ${rc}
 	fi
-	./deploy.sh "${DEPLOY_ROOT}/${repo}" >>${out} 2>&1
+	./deploy.sh "${DEPLOY_ROOT}/${repo}" >${out} 2>&1
 	rc=$?
 	if [ ${rc} -ne 0 ]; then
-		StepMsg "FAIL" "Repository Deploy" "${repo}" "${rc}" "${out}" "${log}" 1
-	else 
+		echo "RepoDeploy: ${repo} deploy failed with rc:${rc}"
+		return ${rc}
+	else
 		rm -f ${out}
-		( 
-		cd "${DEPLOY_ROOT}/${repo}"; 
-		files=`ls -A`
-		pax -x pax -wvf "${DEPLOY_ROOT}/${paxfile}" ${files} >"../${repo}pax.out" 2>&1
-		)
-		rc=$?
-		if [ ${rc} -gt 0 ]; then
-			echo "Unable to create pax file: ${DEPLOY_ROOT}/${paxfile}" >>${out}
-			return ${rc}
-		fi
-		# Tagging binary is required so the file is not autoconverted on curl transfer
-		chtag -b "${DEPLOY_ROOT}/${paxfile}" 
-		urldir="${DEPLOY_SERVER}/${DEPLOY_REPO_PREFIX}${repo}${DEPLOY_REPO_SUFFIX}/${timestamp}"
-		curl -k -T "${DEPLOY_ROOT}/${paxfile}" -u${DEPLOY_USER}:${DEPLOY_API_KEY} -H "X-JFrog-Art-Api:${DEPLOY_API_KEY}" "https://${urldir}/${paxfile}" >>${out} 2>&1
-		rc=$?
-		if [ ${rc} -gt 0 ]; then
-			echo "Unable to transfer pax file: ${DEPLOY_ROOT}/${paxfile} to: ${urldir}/${paxfile}" >>${out}
-			return ${rc}
-		fi
-		curl -k -X POST -u${DEPLOY_USER}:${DEPLOY_API_KEY} -H "X-JFrog-Art-Api:${DEPLOY_API_KEY}" "https://${urldir}/publish" >>${out} 2>&1
-		rc=$?
-		if [ ${rc} -gt 0 ]; then
-			echo "Unable to publish pax file: ${urldir}/${paxfile}" >>${out}
-			return ${rc}
-		fi
-		StepMsg "PASS" "Repository ${repo} deploy git commit hash: ${hash} as ${artifact_url} " "${repo}" "${rc}" "${out}" "${log}" 1
+	fi 
+
+	paxout="${DEPLOY_ROOT}/${repo}_pax.out"
+	( 
+	cd "${DEPLOY_ROOT}/${repo}"; 
+	files=`ls -A`
+	pax -x pax -wvf "${DEPLOY_ROOT}/${paxfile}" ${files} >"${paxout}" 2>&1
+	)
+	rc=$?
+	if [ ${rc} -gt 0 ]; then
+		echo "RepoDeploy: Unable to create pax file: ${DEPLOY_ROOT}/${paxfile}. rc:$rc" 
+		return ${rc}
+	else 
+		rm -f "${paxout}"	
+	fi
+
+	# Tagging binary is required so the file is not autoconverted on curl transfer
+	chtag -b "${DEPLOY_ROOT}/${paxfile}" 
+	urldir="${DEPLOY_SERVER}/${DEPLOY_REPO_PREFIX}${repo}${DEPLOY_REPO_SUFFIX}/${timestamp}"
+	curl -ks -T "${DEPLOY_ROOT}/${paxfile}" -u${DEPLOY_USER}:${DEPLOY_API_KEY} -H "X-JFrog-Art-Api:${DEPLOY_API_KEY}" "https://${urldir}/${paxfile}" >${curlout} 2>&1
+	rc=$?
+	if [ ${rc} -gt 0 ]; then
+		echo "RepoDeploy: Unable to transfer pax file: ${DEPLOY_ROOT}/${paxfile} to: ${urldir}/${paxfile}. rc:$rc"
+		return ${rc}
+	else
+		rm -f ${DEPLOY_ROOT}/${paxfile}
+		rm -f "${curlout}"
+	fi
+
+	curl -ks -X POST -u${DEPLOY_USER}:${DEPLOY_API_KEY} -H "X-JFrog-Art-Api:${DEPLOY_API_KEY}" "https://${urldir}/publish" >${curlout} 2>&1
+	rc=$?
+	if [ ${rc} -gt 0 ]; then
+		echo "RepoDeploy: Unable to publish pax file: ${urldir}/${paxfile}"
+		return ${rc}
+	else
+		rm -f "${curlout}"
 	fi
 	return 0
 }
 
 function RepoDownload {
-	out="RepoDownload.results"
-	log="curl_download.log"
-	rm -f ${out}
-	touch ${out}
 	repo=$1
         paxfile=$2
  	artifact_url=$3
+
+	curlout="curl.out"
+	paxout="pax.out"
+	out="RepoDownload.out"
+	
 	rm -rf "${DOWNLOAD_ROOT}/${repo}"
         mkdir -p "${DOWNLOAD_ROOT}/${repo}"
 	rc=$?
         if [ ${rc} -gt 0 ]; then
-	        echo "Unable to create deploy directory: ${DOWNLOAD_ROOT}/${repo}" >>${out}
+	        echo "RepoDownload: Unable to create deploy directory: ${DOWNLOAD_ROOT}/${repo}. rc:$rc"
 	        return ${rc}
 	fi
 	cd "${DOWNLOAD_ROOT}/${repo}"
-	curl -k -u${DEPLOY_USER}:${DEPLOY_API_KEY} ${artifact_url} -o ${paxfile} >${log} 2>&1
+	curl -ks -u${DEPLOY_USER}:${DEPLOY_API_KEY} ${artifact_url} -o ${paxfile} >${curlout} 2>&1
 	rc=$?
         if [ ${rc} -gt 0 ]; then
-	        echo "Unable to download : ${DOWNLOAD_ROOT}/${repo}/${paxfile}" >>${out}
+	        echo "RepoDownload: Unable to download: ${DOWNLOAD_ROOT}/${repo}/${paxfile}"
 	        return ${rc}
+	else
+		rm -f "${curlout}"
 	fi
-	pax -rf ${paxfile} >>${out}
+	pax -rf ${paxfile} >${paxout} 2>&1
 	rc=$?
         if [ ${rc} -gt 0 ]; then
-	        echo "Unable to unpax : ${DOWNLOAD_ROOT}/${repo}/${paxfile}" >>${out}
+	        echo "RepoDownload: Unable to unpax: ${DOWNLOAD_ROOT}/${repo}/${paxfile}. rc:$rc"
 	        return ${rc}
+	else
+		rm -f "${paxout}"
 	fi
 	DOWNLOAD_ZBREW="../zbrew/bin/zbrew"
 	if [ "${repo}" = "zbrew" ]; then
-		echo "Warning: zbrew itself is not tested directly in download testing" >>${out}
-		StepMsg "PASS" "Download Test" "${repo}" "${rc}" "${out}" "${log}" ${verbose}
 		return 0
 	else
 		if [ -f ${DOWNLOAD_ZBREW} ]; then
@@ -188,37 +191,42 @@ function RepoDownload {
 			export ZBREW_HLQ='ZBRDL.'
 			export ZFSROOT='/zbrdl/'
 
-			# forcibly remove previous builds
-			zfs=`df | grep ${ZFSROOT} | sort -r | awk ' { print $1; }'` 
-			for z in $zfs; do
-				unmount  $z
-			done
-			drm -f "${ZBREW_HLQ}*"
 			for prod in ${prods}; do
-				${DOWNLOAD_ZBREW} install ${prod}
+				# remove previous download builds
+				${DOWNLOAD_ZBREW} uninstall ${prod} >"${out}" 2>&1
 				if [ $rc -gt 0 ]; then
-					echo "Failed to install ${prod} from download" >>${out}
+					echo "RepoDownload: Failed to uninstall ${prod} from download. rc:$rc" 
 					return $rc
 				fi
-				${DOWNLOAD_ZBREW} configure ${prod}
+				${DOWNLOAD_ZBREW} install ${prod} >"${out}" 2>&1
 				if [ $rc -gt 0 ]; then
-					echo "Failed to configure ${prod} from download" >>${out}
+					echo "RepoDownload: Failed to install ${prod} from download. rc:$rc"
+					return $rc
+				fi
+				${DOWNLOAD_ZBREW} configure ${prod} >"${out}" 2>&1
+				if [ $rc -gt 0 ]; then
+					echo "RepoDownload: Failed to configure ${prod} from download. rc:$rc" 
 					return $rc
 				fi
 			done
-		else
-			echo "Warning... zbrew does not exist yet so download testing skipped for ${repo}" >>${out}
+			rm -f "${out}"
 		fi
 	fi
-	StepMsg "PASS" "Download Test" "${repo}" "${rc}" "${out}" "${log}" ${verbose}
 	return 0
-
 }
 
 #
 # Start of mainline 'loop forever'
 #
+first=true
 while true; do 
+	
+	if $first; then     
+		first=false
+	else
+		sleep 5m
+	fi
+
 	mkdir -p "${BUILD_ROOT}"
 	rc=$?
 	if [ ${rc} -gt 0 ]; then
@@ -232,61 +240,61 @@ while true; do
 		exit ${rc}
 	fi
 
-
 	cd "${BUILD_ROOT}"
 	timestamp=`date '+%Y%m%d%H%M'`
 	
 	for r in ${REPO_LIST}; do
-		mkdir -p "${r}"
+		mkdir -p "${BUILD_ROOT}/${r}"
 		rc=$?
 		if [ ${rc} -gt 0 ]; then
 			echo "Unable to	create repository directory: ${BUILD_ROOT}/${r}"
 			exit ${rc}
 		fi
-		cd ${r}
+		cd "${BUILD_ROOT}/${r}"
 		status=`RepoStatus ${r}`
 	
-		log="curl_build.log"
+		log="cicd.log"
 		if [ ${status} = "CURRENT" ]; then
 			if [ ${verbose} -eq 1 ]; then
 				echo "Repository: ${r} current";
 			fi
-		else
-			if [ ${status} = "PULL" ]; then 
-				hash=`git ls-remote -q "${GIT_OWNER}@${GIT_SERVER}:${GIT_USER}/${r}"  | awk ' { if ($2 == "HEAD") { print $1; }}'`
-				echo "Build repository: ${r} (${hash})"
-				SlackMsg "${r}" "0" "Build started for git repository: ${r}" "" "${log}"         
-				status=`RepoBuild ${r}`
-				if [ "${status}" = "PASS" ]; then
-					status=`RepoTest ${r}`
-					if [ ${status} = "PASS" ]; then
-						echo "Repository ${r} test passed"
-						paxfile="${repo}_${timestamp}.pax"
-						artifact_url="https://dl.bintray.com/fultonm/zbrew/${paxfile}"
-						status=`RepoDeploy ${r} ${timestamp} ${hash} ${paxfile} ${artifact_url}`
-						if [ "${status}" = "PASS" ]; then
-							echo "Repository ${r} deployment passed"
-							status=`RepoDownload ${r} ${paxfile} ${artifact_url}`
-							if [ "${status}" = "PASS" ]; then
-								echo "Download of ${r} test passed"
-							else
-								echo "Download of ${r} test failed"
-							fi
-					
-						else
-							echo "Repository ${r} deployment failed"
-						fi
-					else 
-						echo "Repository ${r} test failed"
-					fi
-				else 
-					echo "Build of repository: ${r} failed"
-				fi
-			else 
-				echo "Repository ${r} is out of sync. Investigate!"
-			fi
+			continue
 		fi
-		cd ../
+		if [ ${status} != "PULL" ]; then 
+			echo "Repository ${r} is out of sync. Investigate!"
+			continue
+		fi
+		hash=`git ls-remote -q "${GIT_OWNER}@${GIT_SERVER}:${GIT_USER}/${r}"  | awk ' { if ($2 == "HEAD") { print $1; }}'`
+		echo "Build repository: ${r} (${hash})"
+		SlackMsg "Build started for git repository: ${r}"
+
+		status=`RepoBuild ${r}`
+		rc=$?
+		if [ rc -gt 0 ]; then
+			SlackMsg "${status}"
+			continue
+		fi
+		
+		status=`RepoTest ${r}`
+		rc=$?
+		if [ rc -gt 0 ]; then
+			SlackMsg "${status}"
+			continue
+		fi
+
+		paxfile="${repo}_${timestamp}.pax"
+		artifact_url="https://dl.bintray.com/fultonm/zbrew/${paxfile}"
+		status=`RepoDeploy ${r} ${timestamp} ${hash} ${paxfile} ${artifact_url}`
+		if [ rc -gt 0 ]; then
+			SlackMsg "${status}"
+			continue
+		fi
+
+		status=`RepoDownload ${r} ${paxfile} ${artifact_url}`
+		if [ rc -gt 0 ]; then
+			SlackMsg "${status}"
+		else
+			SlackMsg "Build, test, deploy, download of ${r} passed. Code deployed to: ${artifact_url}"
+		fi
 	done
-	sleep 5m
 done
