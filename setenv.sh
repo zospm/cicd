b40db4df367f8e1c5b73bbb5d87dbdfe127a6313
Mike Fulton <fultonm@ca.ibm.com>
2020-02-15 22:39:38 -0500

    move to general github and bintray organizations

--- setenv.sh
+++ setenv.sh
@@ -4,14 +4,14 @@
 #
 export GIT_SERVER="github.com"
 export GIT_OWNER="git"
-export GIT_USER="mikefultonbluemix"
+export GIT_USER="zbrewdev"
 # order of the repos matters. zbrew requires zbrew-zhw to be installed to run tests
 export REPO_LIST="zbrew-zhw zbrew zbrew-eqa zbrew-igy zbrew-bgz zbrew-fek"
 export DEPLOY_SERVER="api.bintray.com"
 export DEPLOY_USER="fultonm"
 export DEPLOY_REPO_PREFIX="content/fultonm/zbrew/"
 export DEPLOY_REPO_SUFFIX="/"
-export DEPLOY_USER="fultonm"
+export DEPLOY_USER="zbrew"
 export BUILD_ROOT="${HOME}/zbrew-build"
 export DEPLOY_ROOT="${HOME}/zbrew-deploy"
 export DOWNLOAD_ROOT="${HOME}/zbrew-download"
